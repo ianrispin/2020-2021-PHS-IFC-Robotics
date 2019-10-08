c2a98f2b203dc670113ec7ef1cdc666c487488cb
ianrispin <50210259+ianrispin@users.noreply.github.com>
2019-10-08 15:55:05 -0700

    Update color-sensor-value.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/color-sensor-value.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/color-sensor-value.java
@@ -1,6 +1,7 @@
 public class ColorSensorValue extends SensorREVColorDistance {
     public static void main(String[] args) {
         return values[]
+        //returns the values array with the hue, saturation, and value attributes of the color that the sensor is currently reading
     }
 
 }
