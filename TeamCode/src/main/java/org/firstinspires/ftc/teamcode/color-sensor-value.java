9fe82d214b9e5f5196d766af8dc7b6d66c10f9e6
ianrispin <50210259+ianrispin@users.noreply.github.com>
2019-10-10 15:28:33 -0700

    Update color-sensor-value.java
    Fixed issue with getting the color reading.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/color-sensor-value.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/color-sensor-value.java
@@ -1,7 +1,31 @@
 public class ColorSensorValue extends SensorREVColorDistance {
-    public static void main(String[] args) {
-        return values[]
-        //returns the values array with the hue, saturation, and value attributes of the color that the sensor is currently reading
-    }
 
+    public getColor() {
+        // get a reference to the color sensor.
+        sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
+
+        // get a reference to the distance sensor that shares the same name.
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F, 0F, 0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // sometimes it helps to multiply the raw RGB values with a scale factor
+        // to amplify/attentuate the measured values.
+        final double SCALE_FACTOR = 255;
+
+        Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+                (int) (sensorColor.green() * SCALE_FACTOR),
+                (int) (sensorColor.blue() * SCALE_FACTOR),
+                hsvValues);
+
+        return values[]
+    }
+    
+    public static void main(String[] args) {
+
+    }
 }
