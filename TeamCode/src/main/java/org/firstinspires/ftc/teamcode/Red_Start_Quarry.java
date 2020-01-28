b15dba41ec5b3b4f25dfec4cf53f789a4b1d3bbb
ianrispin <50210259+ianrispin@users.noreply.github.com>
2020-01-28 15:31:25 -0800

    implemented holdUnderBridge function in red start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Start_Quarry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Start_Quarry.java
@@ -171,25 +171,25 @@ public class Red_Start_Quarry extends LinearOpMode {
         driveForDistance(0,1,0.65);
         dropHarvester();
         sleep(1000);
-        driveForDistance(0, -1, 0.3);
+        driveForDistance(0, -1, 0.4);
         driveForDistance(1, 0, 1);
 
-        raiseHarvester();
+        holdUnderBridge();
         sleep(1000);
         driveForDistance(-1, 0, 1.2);
-        driveForDistance(0, 1, 0.3);
+        driveForDistance(0, 1, 0.4);
         dropHarvester();
         sleep(1000);
-        driveForDistance(0, -1, 0.3);
+        driveForDistance(0, -1, 0.4);
         driveForDistance(1, 0, 1.2);
 
-        raiseHarvester();
+        holdUnderBridge();
         sleep(1000);
         driveForDistance(-1, 0, 1.4);
-        driveForDistance(0, 1, 0.3);
+        driveForDistance(0, 1, 0.4);
         dropHarvester();
         sleep(1000);
-        driveForDistance(0, -1, 0.3);
+        driveForDistance(0, -1, 0.4);
         driveForDistance(1, 0, 1.4);
 
         raiseHarvester();
